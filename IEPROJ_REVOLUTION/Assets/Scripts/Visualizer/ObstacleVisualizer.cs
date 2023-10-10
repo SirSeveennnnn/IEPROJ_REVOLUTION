8752f454df0086ce046b4f7b51b2ee1989982f89
Meep <79883211+NyipNyerp@users.noreply.github.com>
2023-10-11 01:06:30 +0800

    Added 2 new audio VFX scripts
    - Material Visualizer adds a "pulsing effect" to emissive lighting by adjusting emission intensity
    
    - Object Visualizer adds a "pulsing effect" to object size by adjusting the scale
    
    - both new audioVis scripts inherit from VisualizerBase

---
+++ IEPROJ_REVOLUTION/Assets/Scripts/Visualizer/ObstacleVisualizer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleVisualizer : VisualizerBase
+{
+    private Vector3 startScale;
+    [SerializeField] private float pulseIntensity = 2f;
+    [SerializeField] private float returnSpeed = 2f;
+
+    private void Start()
+    {
+        startScale = transform.localScale;
+    }
+
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+
+        if (isBeat)
+        {
+            return;
+        }
+        transform.localScale = Vector3.Lerp(transform.localScale, startScale, Time.deltaTime * returnSpeed);
+    }
+
+    public override void OnBeat()
+    {
+        base.OnBeat();
+        Pulse();
+    }
+
+    private void Pulse()
+    {
+        Debug.Log("Pulse");
+
+        transform.localScale = startScale * pulseIntensity;
+
+
+        isBeat = false;
+    }
+}
