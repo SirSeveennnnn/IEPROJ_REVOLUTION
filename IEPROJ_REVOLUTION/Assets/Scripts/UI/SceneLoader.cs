8d58ac0b7dea728a9facc17711acb499c8160941
alainroyce <alain_creencia@dlsu.edu.ph>
2023-11-21 14:24:07 +0800

    UI Updates and LEvel 4 added

--- IEPROJ_REVOLUTION/Assets/Scripts/UI/SceneLoader.cs
+++ IEPROJ_REVOLUTION/Assets/Scripts/UI/SceneLoader.cs
@@ -11,6 +11,26 @@ public class SceneLoader : MonoBehaviour
         // Load the "GameScene" when the button is pressed
         SceneManager.LoadScene("MOKKA V2", LoadSceneMode.Single);
     }
+    public void LoadLevel1()
+    {
+        // Load the "GameScene" when the button is pressed
+        SceneManager.LoadScene("ADScene", LoadSceneMode.Single);
+    }
+    public void LoadLevel2()
+    {
+        // Load the "GameScene" when the button is pressed
+        SceneManager.LoadScene("ADScene2", LoadSceneMode.Single);
+    }
+    public void LoadLevel3()
+    {
+        // Load the "GameScene" when the button is pressed
+        SceneManager.LoadScene("ADScene3", LoadSceneMode.Single);
+    }
+    public void LoadLevel4()
+    {
+        // Load the "GameScene" when the button is pressed
+        SceneManager.LoadScene("ADScene4", LoadSceneMode.Single);
+    }
     public void TutorialScene()
     {
         // Load the "GameScene" when the button is pressed
